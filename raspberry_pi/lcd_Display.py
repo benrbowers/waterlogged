8516ada48d8dbbb029f272e2099f0480b3c7caee
BenjamBowers <48335645+BenjamBowers@users.noreply.github.com>
2020-01-25 21:18:14 -0500

    Add files via upload

---
+++ raspberry_pi/lcd_Display.py
@@ -0,0 +1,18 @@
+import board
+import digitalio
+
+
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D27)
+lcd_d6 = digitalio.DigitalInOut(board.D22)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+
+lcd_columns = 16
+lcd_rows = 2
+
+import adafruit_character_lcd.character_lcd as characterlcd
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+lcd.message = "Hello\nCircuitPython!"
