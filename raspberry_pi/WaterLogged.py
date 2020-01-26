bf47b5844ce339963a5afd7c262a952bcfdc239f
aaryakapani <aarya.kapani@gmail.com>
2020-01-25 21:38:03 -0500

    Update WaterLogged.py

--- raspberry_pi/WaterLogged.py
+++ raspberry_pi/WaterLogged.py
@@ -1,6 +1,25 @@
 import RPi. GPIO as GPIO
 import time
+import board
+import digitalio
+
 from httpRequest import http
+
+
+# LCD Setup
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D27)
+lcd_d6 = digitalio.DigitalInOut(board.D22)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+
+lcd_columns = 16
+lcd_rows = 2
+
+import adafruit_character_lcd.character_lcd as characterlcd
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
 
 LED_PIN = 23
 IR_PIN = 18
@@ -25,12 +44,13 @@ url = "http://10.184.41.198/test.php"
 
 while True:
     
-    if minuteEnd - minuteStart >= 15:
+    if minuteEnd - minuteStart >= 60:
         http(elapsedTime, url)
         elapsedTime = 0.0
         minuteStart = time.time()
 
     if not GPIO.input(IR_PIN):
+        lcd.message = "Faucet: ON"
         GPIO.output(LED_PIN, GPIO.HIGH)
         if not waterOn:
             startTime = time.time()
@@ -38,6 +58,7 @@ while True:
         currentTime = time.time()
     else:
         GPIO.output(LED_PIN, GPIO.LOW)
+        lcd.message = "Faucet: OFF"
         currentTime = time.time()
         if waterOn:
             elapsedTime += currentTime - startTime
