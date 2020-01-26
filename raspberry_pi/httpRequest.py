26f03fa6ab0cfd3d53d13b477787910876ad4189
BenjamBowers <48335645+BenjamBowers@users.noreply.github.com>
2020-01-26 00:28:23 -0500

    implented http GET and POST to server

--- raspberry_pi/httpRequest.py
+++ raspberry_pi/httpRequest.py
@@ -1,17 +1,44 @@
 import requests
 import uuid
-
-user = "Ben"
-appliance = "Shower"
+import time
 
 
+def httpPOST(elapsedTime):
+    f = open("userdata.txt", "r")
+    token = f.readline()
+    uid = f.readline()
+    appliance = f.readline()
+    f.close()
 
-def http(elapsedTime, URL):
-    DATA = {
-    'user': user,
-    'appliance': appliance,
-    'elapsedTime': elapsedTime
-}
-    r = requests.post(url = URL, data = DATA)
-    print (r.text)
- 
+    MAC = hex(uuid.getnode())
+    MAC = ':'.join(h[i:i+2] for i in range(2,14,2))
+    
+    timeStamp = time.time()
+
+    URL = "http://waterlogged.appspot.com"
+    URL += '/api/updateData/{}/{}/{}/{}'.format(uid, MAC, elapsedTime, timeStamp)
+
+    headers = {
+        'Authorization': token
+    }
+
+    requests.post(url = URL, data = {}, headers = headers)
+    
+def httpGetToken():
+    MAC = hex(uuid.getnode())
+    MAC = ':'.join(h[i:i+2] for i in range(2,14,2))
+
+    r = requests.get(url = 'waterlogged.appspot.com/pub/token/{}'.format(MAC))
+
+    response = r.json()
+
+    token = response['token']
+    uid = response['uid']
+    appliance = response['appliance']
+
+    open("userdata.txt", "w").close()
+    f = open("userdata.txt", "w")
+    f.write(token)
+    f.write(uid)
+    f.write(appliance)
+    f.close()
